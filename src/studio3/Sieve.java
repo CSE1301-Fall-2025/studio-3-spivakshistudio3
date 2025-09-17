f7d7f244ce9b2acd5f6b6b0bf2d57214f69658d9
sarielaspivak <s.a.spivak@wustl.edu>
2025-09-17 17:10:37 -0500

    Studio3

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Sieve {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("What is n?");
+        int n = in.nextInt();
+        boolean [] array = new boolean[n];
+        for(int i=0; i<array.length; i++){
+            array[i]=true;
+        }
+            for(int i=2; i<n; i++){
+                for(int j=i+i; j<n; j=j+i) {
+                array[j]=false;
+                }    
+            }
+         for(int i=0; i<array.length; i++){
+            if(array[i]==true){
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+//type []name=new type[];
